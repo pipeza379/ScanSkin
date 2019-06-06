b564bfda4cd3e6a35b771a5130bc4696711adbec
pipeza379 <kittapon.ku.ac.th>
2019-06-06 17:48:34 +0700

    add route compare

--- src/route/product/data.js
+++ src/route/product/data.js
@@ -1,145 +1,172 @@
 export const products = [
-{
-    index: 1,
-    img: "",
-    name: "SK-II Facial Treatment Essence",
-    rating: 4,
-    description: {
-        price: 100,
-        quantities: "10g",
-        using: "ss"
+    {
+        index: 1,
+        img: "",
+        name: "SK-II Facial Treatment Essence",
+        rating: 4,
+        type: "",
+        description: {
+            brand: "",
+            price: 100,
+            quantities: "10g",
+            skin: "",
+            using: "ss"
+        },
+        filter: [
+            "fil4"
+        ],
+        hot: true
     },
-    filter: [
-        "fil4"
-    ],
-    hot: true
-},
-{
-    index: 2,
-    img: "",
-    name: "BIOTHERM Life Plankton Essence",
-    rating: 5,
-    description: {
-        price: 150,
-        quantities: "10g",
-        using: "ss"
+    {
+        index: 2,
+        img: "",
+        name: "BIOTHERM Life Plankton Essence",
+        rating: 5,
+        type: "",
+        description: {
+            brand: "",
+            price: 150,
+            quantities: "10g",
+            skin: "",
+            using: "ss"
+        },
+        filter: [
+            "fil2",
+            "fil3"
+        ],
+        hot: true
     },
-    filter: [
-        "fil2",
-        "fil3"
-    ],
-    hot: true
-},
-{
-    index: 3,
-    img: "",
-    name: "ESTEE LAUDER Advanced Night Repair Synchronized Recovery Complex II",
-    rating: 3.5,
-    description: {
-        price: 70,
-        quantities: "10g",
-        using: "ss"
+    {
+        index: 3,
+        img: "",
+        name: "ESTEE LAUDER Advanced Night Repair Synchronized Recovery Complex II",
+        rating: 3.5,
+        type: "",
+        description: {
+            brand: "",
+            price: 70,
+            quantities: "10g",
+            skin: "",
+            using: "ss"
+        },
+        filter: [
+            "fil1",
+            "fil4"
+        ],
+        hot: true
     },
-    filter: [
-        "fil1",
-        "fil4"
-    ],
-    hot: true
-},
-{
-    index: 4,
-    img: "",
-    name: "It's Skin Power 10 Formula VC Effecto",
-    rating: 4.5,
-    description: {
-        price: 130,
-        quantities: "10g",
-        using: "ss"
+    {
+        index: 4,
+        img: "",
+        name: "It's Skin Power 10 Formula VC Effecto",
+        rating: 4.5,
+        type: "",
+        description: {
+            brand: "",
+            price: 130,
+            quantities: "10g",
+            skin: "",
+            using: "ss"
+        },
+        filter: [
+            "fil2",
+            "fil4"
+        ],
+        hot: true
     },
-    filter: [
-        "fil2",
-        "fil4"
-    ],
-    hot: true
-},
-{
-    index: 5,
-    img: "",
-    name: "SK-II Facial Treatment Essence",
-    rating: 4,
-    description: {
-        price: 100,
-        quantities: "10g",
-        using: "ss"
+    {
+        index: 5,
+        img: "",
+        name: "SK-II Facial Treatment Essence",
+        rating: 4,
+        type: "",
+        description: {
+            brand: "",
+            price: 100,
+            quantities: "10g",
+            skin: "",
+            using: "ss"
+        },
+        filter: [
+            "fil4"
+        ],
+        hot: false
     },
-    filter: [
-        "fil4"
-    ],
-    hot: false
-},
-{
-    index: 6,
-    img: "",
-    name: "BIOTHERM Life Plankton Essence",
-    rating: 5,
-    description: {
-        price: 150,
-        quantities: "10g",
-        using: "ss"
+    {
+        index: 6,
+        img: "",
+        name: "BIOTHERM Life Plankton Essence",
+        rating: 5,
+        type: "",
+        description: {
+            brand: "",
+            price: 150,
+            quantities: "10g",
+            skin: "",
+            using: "ss"
+        },
+        filter: [
+            "fil2",
+            "fil3"
+        ],
+        hot: false
     },
-    filter: [
-        "fil2",
-        "fil3"
-    ],
-    hot: false
-},
-{
-    index: 7,
-    img: "",
-    name: "ESTEE LAUDER",
-    rating: 3.5,
-    description: {
-        price: 70,
-        quantities: "10g",
-        using: "ss"
+    {
+        index: 7,
+        img: "",
+        name: "ESTEE LAUDER",
+        rating: 3.5,
+        type: "",
+        description: {
+            brand: "",
+            price: 70,
+            quantities: "10g",
+            skin: "",
+            using: "ss"
+        },
+        filter: [
+            "fil1",
+            "fil4"
+        ],
+        hot: false
     },
-    filter: [
-        "fil1",
-        "fil4"
-    ],
-    hot: false
-},
-{
-    index: 8,
-    img: "",
-    name: "It's Skin Power 10 Formula VC Effecto",
-    rating: 4.5,
-    description: {
-        price: 130,
-        quantities: "10g",
-        using: "ss"
+    {
+        index: 8,
+        img: "",
+        name: "It's Skin Power 10 Formula VC Effecto",
+        rating: 4.5,
+        type: "",
+        description: {
+            brand: "",
+            price: 130,
+            quantities: "10g",
+            skin: "",
+            using: "ss"
+        },
+        filter: [
+            "fil2",
+            "fil4"
+        ],
+        hot: false
     },
-    filter: [
-        "fil2",
-        "fil4"
-    ],
-    hot: false
-},
-{
-    index: 9,
-    img: "",
-    name: "SK-II Facial Treatment Essence",
-    rating: 4,
-    description: {
-        price: 100,
-        quantities: "10g",
-        using: "ss"
-    },
-    filter: [
-        "fil4"
-    ],
-    hot: true
-}
+    {
+        index: 9,
+        img: "",
+        name: "SK-II Facial Treatment Essence",
+        rating: 4,
+        type: "",
+        description: {
+            brand: "",
+            price: 100,
+            quantities: "10g",
+            skin: "",
+            using: "ss"
+        },
+        filter: [
+            "fil4"
+        ],
+        hot: true
+    }
 ]
 
 function filterHotProduct() {
