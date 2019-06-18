6888b9e361773da4c2d14d0003dbfec8f20ef075
pipeza379 <kittapon.ku.ac.th>
2019-06-19 00:54:38 +0700

    .

--- src/components/home/homeCarousel.js
+++ src/components/home/homeCarousel.js
@@ -1,12 +1,12 @@
 import React from "react"
 import { Carousel } from "antd"
-import slide1 from '../../data/img/home/banner4.png'
+// import slide1 from '../../data/img/home/banner4.png'
 
 export default function HomeCarousel() {
     return (
         <Carousel autoplay={true}>
             <div className="slide1">
-                <img style={{ width: "100%" }} alt="curousel" src={slide1} />
+                {/* <img style={{ width: "100%" }} alt="curousel" src={slide1} /> */}
             </div>
         </Carousel>
     )
