f827ba84edacf0b0dc923b6bfa1c2862f756e08b
pipeza379 <kittapon.j@ku.th>
2019-07-22 02:43:24 +0700

    change to browser router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './asset/css/index.css'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker'
 import Router from './Router'
 import { createStore, applyMiddleware } from 'redux';
@@ -13,9 +13,9 @@ const store = createStore(reducer, applyMiddleware(logger))
 
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <Router />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
   , document.getElementById('root')
 )
